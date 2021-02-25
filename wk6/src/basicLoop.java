2a15ad2ac1e330de43b73567c5936d6eec434bd4
s550 <deviltrigger87@gmail.com>
2021-02-24 18:08:46 -0800

    wk 6 hw start

---
+++ wk6/src/basicLoop.java
@@ -0,0 +1,7 @@
+public class basicLoop {
+    public static void main(String[] args){
+        for (int i = 0; i <= 999; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ wk6/src/commaLoop.java
@@ -0,0 +1,12 @@
+public class commaLoop {
+    public static void main(String[] args){
+        for(int i = 1; i <= 100; i++){
+            if(i <= 99){
+                System.out.print(i + ", ");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
